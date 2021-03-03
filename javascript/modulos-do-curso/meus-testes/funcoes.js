3361b0fc24229f375984a1fbae144dbecbb13fd8
Leonardo Delfino <leonardossdelfino@gmail.com>
2021-03-03 17:57:44 -0300

    revisão de aulas

---
+++ javascript/modulos-do-curso/meus-testes/funcoes.js
@@ -0,0 +1,17 @@
+const somar = (a, b) => {
+    return a + b
+}
+
+console.log(somar(2, 3))
+
+const escreva = (texto) => {
+    console.log(texto)
+}
+
+escreva('Leo')
+
+const media = (nota1, nota2, nota3, nota4) => {
+    return (nota1 + nota2 + nota3 + nota4) / 4
+}
+
+console.log('A média é: ' + media(2, 3, 5, 6))
