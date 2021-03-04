b39cb2255bc9bb1f0e102dc6a6d5cdd44354c3ff
Leonardo Delfino <leonardossdelfino@gmail.com>
2021-03-04 11:53:08 -0300

    revisão de aulas

--- javascript/modulos-do-curso/fundamentos/aritmeticos.js
+++ javascript/modulos-do-curso/fundamentos/aritmeticos.js
@@ -5,7 +5,7 @@ const [a, b, c, d] = [3, 5, 1, 15]
 
 const soma = a + b + c + d
 const subtracao = d - b
-const multiplicacao = d * a
+const multiplicacao = a * b
 const divisao = d / a
 const modulo = a % 2
 
