2d61302eb1590fc82dc49a3c2c022457643f2d27
Leonardo Delfino <leonardossdelfino@gmail.com>
2021-03-17 10:12:08 -0300

    Revisão de funções, quais são a formas de declarar uma função.

--- javascript/modulos-do-curso/funcao/cidadaoPrimeiraClasse.js
+++ javascript/modulos-do-curso/funcao/cidadaoPrimeiraClasse.js
@@ -1,5 +1,7 @@
 // Função em JS é First-Class Object (Citizens)
 // Higher-order function (Função de primeira ordem)
+// Quando a função é tratada como um DADO
+// Usado muito em programação Funcional
 
 // criar de forma literal
 function fun1() {
@@ -8,13 +10,18 @@ function fun1() {
 
 // armazenar em uma variável
 // quando não damos nome na função ela se torna anônima
+// chamando a constante e passando () ele vai invocar a função
+// armaazenada nela
 const fun2 = function () {
 
 }
 
 // armazenar em um array
+// posso declarar diretamente a função, ou armazenar a função 
+// literal, por exemplo fun1 e fun2 criada anteriormente
 const array = [function (a, b) { return a + b}, fun1, fun2]
-// para invocar passo o índice do array e o valores solicitados pela função a + b
+// para invocar passo o índice do array e o valores solicitados 
+// pela função dentro do parenteses a + b
 console.log(array[0](2, 3))
 
 // armazenar em um atributo de objeto
@@ -22,12 +29,25 @@ const obj = {} // objeto vazio
 obj.falar = function () { return 'Opa!'} // crio o atribuito falar dentro de obj
 console.log(obj.falar())
 
+// ou
+
+const obj2 = {
+    falar: function () {
+        return 'Opa!'
+    }
+}
+
+console.log(obj2.falar)
+
 // passar a função como parametro
+// para executar dentro de outra função
 function run(fun) {
     fun()
 }
 
-run(function () { console.log('Executando...')})
+run(function () { 
+    console.log('Executando...')
+})
 
 // uma função pode retornar/conter uma função
 function soma(a, b) {
@@ -36,8 +56,9 @@ function soma(a, b) {
     }
 }
 
-// como o resultado da função soma é uma função (a+b), eu posso logo em seguida
-// passar o valor solcitado pela outra função(c)
+// como o resultado da função soma é uma função (a+b), 
+// eu posso logo em seguida passar o valor solcitado 
+// pela outra função(c)
 soma(2, 3)(4)
 
 // ou
